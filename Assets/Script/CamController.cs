76af284b50aba2da389a524f11f554949805a6f5
dark_luminex <jyotip.m05@gmail.com>
2025-08-17 10:03:34 +0530

    change

---
+++ Assets/Script/CamController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Script
+{
+    public class CamController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [Header("Camera Settings")] 
+        [SerializeField] private InputActionAsset input;
+        [SerializeField] private float lookSensitivity = 2f;
+
+        [Header("Actions Maps")] [SerializeField]
+        private InputAction look;
+    
+        void Start()
+        {
+            look = input.FindActionMap("Player").FindAction("Look");
+        }
+    
+        private void OnEnable()
+        {
+            look.Enable();
+        }
+    
+        private void OnDisable()
+        {
+            look.Disable();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float upDown = look.ReadValue<Vector2>().y * lookSensitivity * Time.deltaTime;
+            transform.Rotate(0f,0f, upDown);
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,43 +3,91 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset inputActions;
+    [Header("Movement")] [SerializeField] private InputActionAsset inputActions;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
 
-    private InputAction moveAction;
-    private InputAction jumpAction;
-    private Rigidbody rb;
+    [Header("Look Settings")] [SerializeField]
+    private float lookSensitivity = 2f;
 
-    void Awake()
+    [SerializeField] private Transform cameraTransform;
+    private float cameraPitch = 0f;
+
+    [Header("Input Actions")] InputAction _moveAction;
+    InputAction _jumpAction;
+    InputAction _lookAction;
+
+    [Header("References")] private Rigidbody rb;
+
+    [Header("Ground Check")] [SerializeField]
+    private float groundCheckDistance = 0.1f;
+
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded = true;
+
+    private void Awake()
     {
+        // Find the references to the "Move" and "Jump" actions from the asset
+        _moveAction = inputActions.FindActionMap("Player").FindAction("Move");
+        _jumpAction = inputActions.FindActionMap("Player").FindAction("Jump");
+        _lookAction = inputActions.FindActionMap("Player").FindAction("Look");
         rb = GetComponent<Rigidbody>();
-        var playerMap = inputActions.FindActionMap("Player");
-        moveAction = playerMap.FindAction("Move");
-        jumpAction = playerMap.FindAction("Jump");
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
-        moveAction?.Enable();
-        jumpAction?.Enable();
+        _moveAction.Enable();
+        _jumpAction.Enable();
+        _lookAction.Enable();
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
-        moveAction?.Disable();
-        jumpAction?.Disable();
+        _moveAction.Disable();
+        _jumpAction.Disable();
+        _lookAction.Disable();
     }
 
     void Update()
     {
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * (speed * Time.deltaTime);
-        transform.Translate(move, Space.World);
+        // Ground check
+        // isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance + 0.1f, groundMask);
 
-        if (jumpAction.triggered && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        // Read the "Move" action value, which is a 2D vector
+        Vector2 moveValue = _moveAction.ReadValue<Vector2>();
+        // Move relative to player orientation
+        Vector3 moveDirection = transform.right * moveValue.x + transform.forward * moveValue.y;
+
+        // Move using Rigidbody for better physics
+        Vector3 velocity = moveDirection * speed;
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+
+        if (_jumpAction.triggered && isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Jump();
         }
+        HandleLook();
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void HandleLook()
+    {
+        Vector2 lookValue = _lookAction.ReadValue<Vector2>() * lookSensitivity;
+
+        // Rotate player left/right (yaw)
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, lookValue.x, 0f));
+
+        // Camera up/down (pitch)
+        cameraPitch -= lookValue.y;
+        // cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+        // if (cameraTransform is not null)
+        // {
+        //     cameraTransform.localEulerAngles = new Vector3(cameraPitch, 0f, 0f);
+        // }
     }
 }
